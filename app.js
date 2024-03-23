b96c489c91218658c71984a0308e713a32b0bf77
muhammadkashif20 <kashif28@gmail.com>
2024-03-22 23:35:31 -0700

    1 LINE WORK

--- app.js
+++ app.js
@@ -1,16 +1,62 @@
-//Pattern No 01:-
+// Number Pattern No 01:-
 document.write('<h5>Pattern No 01:-</h5>')
-for(var i=0;i<7;i++){
-    document.write('<br>')
-    for(var j=1;j<i+2;j++){
-        document.write(j)
-    }
-}
-//Pattern No 02:-
-document.write('<h5>Pattern No 02:-</h5>')
 for(var i=0;i<=7;i++){
     document.write('<br>')
     for(var j=1;j<i+1;j++){
         document.write(j)
     }
 }
+//Number Pattern No 02:-
+document.write('<h5>Pattern No 02:-</h5>')
+for(var i=6;i>=0;i--){
+    document.write('<br>')
+    for(var j=1;j<=i+1;j++){
+        document.write(j)
+    }
+}
+//Star  Pattern No 01:-
+document.write('<h5> Star Pattern No 01:-</h5>')
+for(var i=0;i<=7;i++){
+    document.write('<br>')
+    for(var j=1;j<i+1;j++){
+        document.write('*')
+    }
+}
+//Star  Pattern No 02:-
+document.write('<h5> Star Pattern No 02:-</h5>')
+for(var i=6;i>=0;i--){
+    document.write('<br>')
+    for(var j=1;j<i+1;j++){
+        document.write('*')
+    }
+}
+//Characters  Pattern No 01:-
+document.write('<h5> Characters Pattern No 01:-</h5>')
+
+
+
+//Pattern No 03:-
+document.write('<h5>Pattern No 03:-</h5>')
+for(var i=0;i<=7;i++){
+    document.write('<br>')
+    for(var j=1;j<i+1;j++){
+        document.write(j)
+    }
+}
+//Pattern No 04:-
+document.write('<h5>Pattern No 04:-</h5>')
+for(var i=6;i>=0;i--){
+    document.write('<br>')
+    for(var j=1;j<=i+1;j++){
+        document.write(j)
+    }
+}
+//Pattern No 05:-
+document.write('<h5>Pattern No 05:-</h5>')
+for(var i=0;i<=7;i++){
+    document.write('<br>')
+    for(var j=1;j<i+1;j++){
+        document.write('A')
+    }
+}
+
